5ae76255bd5a1bf1aa8f540a938988709a7880e1
Claudéric <clauderic.deroy@umontreal.ca>
2022-07-28 10:08:12 -0400

    added install-require to setup.py so that it automatically install on docker

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(name='seed_to_voxel',
       author='Claudéric DeRoy Xanthy Lajoie',
       author_email='clauderic.deroy@umontreal.ca xanthy.lajoie@umontreal.ca',
       url='https://github.com/brainhack-school2022/deroy_project'
+      install_requires=["matplotlib==3.3.4", "nilearn==0.9.1", "numpy==1.20.1", "pandas==1.4.2"]
 )
