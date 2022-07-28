2788134be31c3a896a7600d9d5c8d6a8d3e73b75
Claudéric <clauderic.deroy@umontreal.ca>
2022-07-28 10:19:01 -0400

    small modif for packaging

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='seed_to_voxel',
       description='This package is to be used for seed to voxel correlation application',
       author='Claudéric DeRoy Xanthy Lajoie',
       author_email='clauderic.deroy@umontreal.ca xanthy.lajoie@umontreal.ca',
-      url='https://github.com/brainhack-school2022/deroy_project'
+      url='https://github.com/brainhack-school2022/deroy_project',
       install_requires=["matplotlib==3.3.4", "nilearn==0.9.1", "numpy==1.20.1", "pandas==1.4.2"]
 )
